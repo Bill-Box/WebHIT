8e5886d4edaaf171b2cb66d57a47853aeec672d0
Tên của bạn <email@example.com>
2025-12-17 15:37:58 +0700

    BTVNW9

--- Week10/W10.js
+++ Week10/W10.js
@@ -83,25 +83,19 @@
 //     }  
 // }
      document.getElementById("form").addEventListener("submit", function(event) {
-  event.preventDefault(); // Ngừng sự kiện gửi form mặc định
-
+  event.preventDefault(); 
   const name = document.getElementById("name").value;
-  const score = document.getElementById("score").value;
-
+  const score = Number(document.getElementById("score").value); 
   if (name && score) {
-    // Tạo phần tử hiển thị sinh viên
     const studentDiv = document.createElement("div");
     studentDiv.classList.add("student-item");
-
-    studentDiv.innerHTML = `<span>${name}</span> - ${score} điểm`;
-
-    // Thêm vào danh sách sinh viên
+    const color = score >= 5 ? "blue" : "red"; 
+    studentDiv.innerHTML = `<span style="color: ${color};">${name} - ${score} điểm</span>`;
     document.getElementById("studentList").appendChild(studentDiv);
-
-    // Xóa thông tin trong form
     document.getElementById("name").value = "";
     document.getElementById("score").value = "";
   } else {
     alert("Vui lòng nhập đầy đủ thông tin!");
   }
 });
+
